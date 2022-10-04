932a4ade1707bfdf1f74daf7f5d63b60b9b5aceb
dmilleza <darrenmilleza@gmail.com>
2022-10-03 20:11:04 -0700

    planning

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,23 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+
+
+
+// 1. Prompt user for the password criteria 
+//  a. Password length 8 < 128
+//  b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+// 4. Display password to the page
 
 // Write password to the #password input
 function writePassword() {
